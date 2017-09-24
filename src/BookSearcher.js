de68c16c44b80eb2265a9f9ace00d0beb943c380
wengelen@gmail.com <wengellen@gmail.com>
2017-09-24 12:43:36 -0700

    Add local and external font [ Cleanup CSS # 151161160]

--- src/BookSearcher.js
+++ src/BookSearcher.js
@@ -6,54 +6,54 @@
  import { Link } from 'react-router-dom'
  
 class BookSearcher extends Component {
-  state={
-  	query: ''
-  }
-  
-  updateQuery = (query) =>{
-    this.setState({query: query.trim()})
-    this.props.onSearch(query, 10 )
-  }
+	state={
+		query: ''
+	}
 	
-  
-  clearQuery = () => {
-   this.setState({query: ''})
-  }
+	updateQuery = (query) =>{
+		this.setState({query: query.trim()})
+		this.props.onSearch(query, 10 )
+	}
+	
+	// TODO: Add paging
+	clearQuery = () => {
+		this.setState({query: ''})
+	}
 	
 	onBookStatusChange =(book, newShelf) => {
 		book.shelf = newShelf
 		let res = this.props.onUpdate(book, newShelf)
-		// this.setState({books: this.props.books})
 	}
 	
 	render(){
-    const {onSearch, searchResults, onAddBookToShelf } = this.props
-    const {query, } = this.state
+		const {onSearch, searchResults, onAddBookToShelf } = this.props
+		const {query, } = this.state
 		
-	let alreadySaved = result => {
-		let res = this.props.books.filter(book => result.id === book.id)
-		console.log(res)
-		return res
+		let alreadySaved = result => {
+			let res = this.props.books.filter(book => result.id === book.id)
+			console.log(res)
+			return res
+		}
+		return(
+		   <div className="search-container">
+			   <div className="back-button">
+				   <Link to="/">Back</Link>
+			   </div>
+			   <input className="search-bar"
+					  value={query}
+					  type="text"
+					  placeholder="Search by title or author"
+					  onChange={(event) => this.updateQuery(event.target.value)}/>
+				
+			   <div className="seach-book-list">
+				   {this.props.searchResults.map((book) =>
+					  <Book key={book.id} book={book} onStatusChange= {this.onBookStatusChange}
+					  disabled={alreadySaved(book).length >0 }/>
+				   )}
+			   </div>
+		   </div>
+	    )
 	}
-   return(
-	   <div className="search-container">
-		   <div className="back-button">
-			   <Link to="/">Back</Link>
-		   </div>
-		   <input className="search-bar"
-		          value={query}
-				  type="text"
-		          onChange={(event) => this.updateQuery(event.target.value)}/>
-			
-		   <div className="seach-book-list">
-			   {this.props.searchResults.map((book) =>
-			      <Book key={book.id} book={book} onStatusChange= {this.onBookStatusChange}
-			      disabled={alreadySaved(book).length >0 }/>
-			   )}
-		   </div>
-	   </div>
-   )
-  }
 }
 
 export default BookSearcher
